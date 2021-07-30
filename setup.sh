558a559ca749f19fd487f9a2ed65a551ccefc80a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-07-30 06:27:58 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# add a section for installing pacman list or something and bootstrapping yay
+# and basically reimplement everything in larbs???
+
 mkdir -p ../.config/Code/User
 mkdir -p ../.local/bin
 mkdir -p ../.local/share/nvim/site/autoload
