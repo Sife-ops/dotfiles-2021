936e1f83d7eacc76e6dd1c6f868fd080207fb599
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-07 18:28:52 -0700

    update

--- scripts/.local/bin/bookmarks.sh
+++ scripts/.local/bin/bookmarks.sh
@@ -1,250 +1,211 @@
 #!/bin/sh
-# bookmarks manager using sqlite3
 
 self=$(basename "${0##/*/}")
 
-dataDir="${XDG_DATA_HOME}/bookmarks"
+dbDir="${XDG_DATA_HOME}/bookmarks"
+# dbDir="${HOME}/.local/src/bookmarks"
 dbName="bookmarks.db"
-db="${dataDir}/${dbName}"
+db="${dbDir}/${dbName}"
 
-config="${XDG_CONFIG_HOME}/bookmarks/bookmarksrc"
-while IFS="" read -r line; do
-    eval "$line"
-done < "$config"
+bookmarkList(){
+    sqlite3 "$db" "
+        select
+            description,
+            url,
+            rating,
+            bookmark_id
+        from bookmark;
+    "
+}
 
-# createDb () { #^
-# } #$
+bookmarkTagList(){
+    sqlite3 "$db" "
+        select
+            b.description,
+            b.url,
+            b.rating,
+            b.bookmark_id
+        from bookmark_tag
+        inner join bookmark b using (bookmark_id)
+        where tag_id = (
+            select tag_id from tag
+            where name = '$1'
+        );
+    "
+}
 
-download () { #^
-    pushd "$dataDir"
-    eval "$fetchCmd" &&
-        notify-send "$self" "Download successful." ||
-        notify-send "$self" "Download failed."
-    tar xf "${dbName}.tar.xz"
-    popd
-} #$
+sepStr="="
+sep () {
+    i=0
+    while [ $i -le 500 ]; do
+        printf "%s" "$sepStr"
+        i=$(( i + 1 ))
+    done
+    printf "\n"
+}
 
-upload () { #^
-    pushd "$dataDir"
-    tar caf ${dbName}.tar.xz  ${dbName}
-    eval "$pushCmd" &&
-        notify-send "$self" "Upload successful." ||
-        notify-send "$self" "Upload failed."
-    popd
-} #$
+tagsView(){
+    echo "back"
+    sep
+    sqlite3 "$db" "select name from tag;"
+    sep
+    if [ "$tag" = "" ]; then
+        bookmarkList
+    else
+        bookmarkTagList "$tag"
+    fi
+}
 
-tags_list () { #^
-    sqlite3 "$db" "SELECT name
-        FROM sqlite_master
-        WHERE type = 'table'
-        AND name
-        NOT LIKE 'sqlite_%'
-        AND name
-        NOT LIKE 'bookmark';"
-} #$
+mainView(){
+    echo "create"
+    echo "tags"
+    sep
+    bookmarkList
+}
 
-main_list () { #^
-    # echo "==== Actions ==================================================================================================================================================================================================================================================================================================================================================================================================="
-    echo "create bookmark"
-    echo "create tag"
-    echo "download"
-    echo "upload"
-    echo "==== Tags ======================================================================================================================================================================================================================================================================================================================================================================================================"
-    tags_list | sort
-    echo "==== All ======================================================================================================================================================================================================================================================================================================================================================================================================="
-    sqlite3 "$db" "SELECT description,url
-        FROM bookmark;"
-    echo "sqlitebrowser"
-} #$
-
-action_list () { #^
-    echo "open in browser"
-    echo "open with url.sh"
-    [ -n "$1" ] && echo "remove tag" || echo "add tag"
+urlView(){
+    echo "open"
+    echo "copy"
+    echo "rate"
+    echo "add tag"
+    if [ "$viewPrev" = "tags" ] && [ "$tag" != "" ]; then
+        echo "remove tag"
+    fi
     echo "delete"
-} #$
+}
 
-filter_list () { #^
-    # echo "open all in browser"
-    # echo "================================================================================================================================================================================================================================================================================================================================================================================================================"
-    sqlite3 "$db" "SELECT description,url,bookmarkId
-        FROM bookmark
-        WHERE bookmarkId
-        IN (SELECT bookmarkId FROM '${tag}');"
-    echo "================================================================================================================================================================================================================================================================================================================================================================================================================"
-    echo "delete tag"
-} #$
-
-create_tag () { #^
-    # tag name -> $1
-    sqlite3 "$db" "CREATE TABLE '${tag}' (
-        '${tag}Id'	INTEGER NOT NULL UNIQUE,
-        'bookmarkId'	INTEGER NOT NULL UNIQUE,
-        PRIMARY KEY('${tag}Id' AUTOINCREMENT),
-        FOREIGN KEY('bookmarkId') REFERENCES 'bookmark'('bookmarkId') ON DELETE CASCADE
-    );"
-} #$
-
-delete_tag () { #^
-    sqlite3 "$db" "drop table '${tag}';"
-} #$
-
-add_tag () { #^
-    sqlite3 "$db" "INSERT INTO '${tag}' (bookmarkId)
-    VALUES ((SELECT bookmarkId
-        FROM bookmark
-        WHERE url = '${url}'));"
-} #$
-
-remove_tag () { #^
-    sqlite3 "$db" "DELETE FROM '${tag}'
-        WHERE bookmarkId = '${bookmarkId}';"
-} #$
-
-delete_bookmark () { #^
-    sqlite3 "$db" "DELETE FROM bookmark
-    WHERE url = '${url}';"
-} #$
-
-create_bookmark () { #^ #^
+create(){
     url="$(xclip -o -selection clipboard)"
 
-    tmp=$(mktemp /tmp/bookmarks_XXXXX)
+    tmp=$(mktemp /tmp/XXXXXX)
     curl -L "$url" -o "$tmp"
     description="$(egrep -o '<title>.*</title>' "$tmp" | head -n 1)"
     description="$(echo "$description" | sed -E 's/(<title>)(.*)(<\/title>)/\2/')"
     description="$(echo "$description" | sed 's/|/:/')"
-    rm "$tmp"
 
-    tmp=$(mktemp /tmp/bookmarks_XXXXX)
     echo "{
-        \"url\":\"${url}\",
         \"description\":\"${description}\",
-        \"tags\": [
-            \"<++>\",
-            \"<++>\"
-        ]
+        \"url\":\"${url}\"
     }" | jq "." > "$tmp"
 
     $TERMEXEC $EDITOR "$tmp"
     jq '.' "$tmp" || kill 0
-    # while ! cat "$tmp" | jq; do
-    #     $TERMEXEC $EDITOR "$tmp"
-    # done
 
     url="$(jq -r '.url' "$tmp")"
     description="$(jq -r '.description' "$tmp")"
     rating="$(jq -r '.rating' "$tmp")"
 
-    [ "$url" = "null" ] || \
-        [ "$description" = "null" ] && \
-        kill 0
-
-    sqlite3 "$db" \
-        "INSERT INTO bookmark (url, description)
-        VALUES ('${url}', '${description}');"
-
-    if jq -r 'has("tags")' "$tmp"; then
-        bookmarkId=$(sqlite3 "$db" \
-            "SELECT bookmarkId
-            FROM bookmark
-            WHERE url = '$url';")
-        jq -r '.tags[]' "$tmp" |
-            while IFS="" read -r tag; do
-                tag="#${tag}"
-                sqlite3 "$db" \
-                    "INSERT INTO '${tag}' (bookmarkId)
-                    VALUES ($bookmarkId);"
-            done
-    fi
     rm "$tmp"
-} #$
 
-url_menu () { #^
-    case "$chosen" in
-        "open in browser") $BROWSER "$url" ;;
-        "open with url.sh") url.sh "$url" ;;
-        "add tag") tag="$(tags_list | ${DMENU_CMD:-dmenu})"; [ "$tag" = "" ] && exit 1
-            add_tag ;;
-        "remove tag") remove_tag ;;
-        delete) delete_bookmark ;;
-    esac
-} #$
+    if [ "$url" = "" ] || [ "$description" = "" ]; then
+        kill 0
+    fi
 
-confirm () { #^
-    printf "yes\nno" |
-        ${DMENU_CMD:-dmenu} -p "Are you sure?" |
-        grep 'yes' 1>/dev/null 2>&1 &&
-            true ||
-            false
-} #$
+    sqlite3 "$db" "
+        insert into bookmark (url, description)
+        values ('${url}', '${description}');
+    "
+}
 
-[ -f "$db" ] || download
-if [ ! -f "$db" ]; then
-    notify-send "${0##/*/}" "failed to download database"
-    exit 1
-fi
+view="main"
+viewPrev=""
+tag=""
+url=""
+while true; do
 
-chosen="$(main_list | ${DMENU_CMD:-dmenu})"; [ "$chosen" = "" ] && exit 1
-case "$chosen" in
-    "create bookmark") create_bookmark ;;
-    "create tag")   tag="$(printf "" |
-                        ${DMENU_CMD:-dmenu} -p "tag name" |
-                        sed -E 's/(.*)/#\1/')"
-                    create_tag ;;
-    sqlitebrowser) sqlitebrowser "$db" & ;;
-    upload) confirm && upload ;;
-    download) confirm && download ;;
-    '#'*)   tag="$chosen"
-            chosen="$(filter_list | ${DMENU_CMD:-dmenu})"; [ "$chosen" = "" ] && exit 1
+    case "$view" in
+
+        "main")
+
+            chosen=$(
+                mainView | dmenu -i -l 10 $DMENU_OPTS
+            ); [ "$chosen" = "" ] && exit 1
+
             case "$chosen" in
-                # "open all in browser") delete_tag ;;
-                "delete tag") delete_tag ;;
-                *)  url="$(echo "$chosen" | cut -d '|' -f 2)"
-                    bookmarkId="$(echo "$chosen" | cut -d '|' -f 3)"
-                    chosen="$(action_list t | ${DMENU_CMD:-dmenu})"; [ "$chosen" = "" ] && exit 1
-                    url_menu ;;
-            esac ;;
-    *)  url="$(echo "$chosen" | cut -d '|' -f 2)"
-        chosen="$(action_list | ${DMENU_CMD:-dmenu})"; [ "$chosen" = "" ] && exit 1
-        url_menu ;;
-esac
+                "create")
+                    create || exit 1
+                    exit
+                    ;;
+                "tags")
+                    view="tags"
+                    viewPrev="main"
+                    ;;
+                "="*)
+                    exit 1
+                    ;;
+                *)
+                    url=$(echo "$chosen" | cut -d '|' -f 2)
+                    view="url"
+                    viewPrev="main"
+                    ;;
+            esac
 
-#^ The Categorization Script:
-##!/bin/bash
+            ;;
 
-#db="./bookmarks.db"
+        "tags")
 
-#tags="$(
+            chosen=$(
+                tagsView | dmenu -i -l 10 $DMENU_OPTS
+            ); [ "$chosen" = "" ] && exit 1
 
-#        sqlite3 "$db" "SELECT name
-#            FROM sqlite_master
-#            WHERE type = 'table'
-#            AND name
-#            NOT LIKE 'sqlite_%'
-#            AND name
-#            NOT LIKE 'bookmark';" | nl
+            case "$chosen" in
+                "back")
+                    view="main"
+                    viewPrev="tags"
+                    ;;
+                "#"*)
+                    tag="$chosen"
+                    ;;
+                "="*)
+                    exit 1
+                    ;;
+                *)
+                    url=$(echo "$chosen" | cut -d '|' -f 2)
+                    view="url"
+                    viewPrev="tags"
+                    ;;
+            esac
 
-#)"
+            ;;
 
-#while IFS="|" read a url c; do
-#    # url="$(echo "$url" | tr -d '[:space:]')" # breaks everything
-#    while true; do
-#        echo "$tags"
-#        echo "=============================================================================="
-#        echo "${url}"
-#        echo "$c"
-#        read -p "n:next " act </dev/tty
-#        case "$act" in
-#            n) break ;;
-#            *)  tag="$(echo "$tags" | sed -n "${act}p" | awk '{print $2}')"
-#                sqlite3 "$db" "INSERT INTO '${tag}' (bookmarkId)
-#                VALUES ((SELECT bookmarkId
-#                    FROM bookmark
-#                    WHERE url = '${url}'));" ;;
-#        esac
-#    done
-#done < <(sqlite3 "$db" "select * from bookmark;")
-#$
+        "url")
 
-# vim: fdm=marker fmr=#^,#$
+            chosen=$(
+                urlView | dmenu -i -l 10 $DMENU_OPTS
+            ); [ "$chosen" = "" ] && exit 1
+
+            case "$chosen" in
+                "open")
+                    $BROWSER $url
+                    exit
+                    ;;
+                "copy")
+                    echo "$url" | xclip -i -selection clipboard
+                    exit
+                    ;;
+                "rate")
+                    echo "rate action"
+                    exit
+                    ;;
+                "add tag")
+                    echo "add tag action"
+                    exit
+                    ;;
+                "remove tag")
+                    echo "remove tag action"
+                    exit
+                    ;;
+                "delete")
+                    echo "delete action"
+                    exit
+                    ;;
+                *)
+                    exit 1
+                    ;;
+            esac
+
+            ;;
+
+    esac
+
+done
