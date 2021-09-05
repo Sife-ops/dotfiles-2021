2f06b8a44d70cbaa25154379629171f05f2bb32e
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-09-04 23:33:55 -0700

    update

--- scripts/.local/bin/setxmodmap.sh
+++ scripts/.local/bin/setxmodmap.sh
@@ -5,8 +5,10 @@ setxkbmap -option
 # killall -e xcape 1>/dev/null 2>&1
 
 if [ ! "$1" = "default" ]; then
+    setxkbmap -option caps:escape
     xmodmap "$XMODMAP"
     # xcape -e 'Alt_L=Tab;Control_L=Escape;Alt_R=backslash;Control_R=BackSpace' &
 fi
+
 
 # xset r rate 300 50
