a7045385ab9551dae8207eba3da9e301aa95d084
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-09-04 21:28:48 -0700

    update

--- scripts/.local/bin/setxmodmap.sh
+++ scripts/.local/bin/setxmodmap.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-# set up keyboard stuff
-
-# checkdeps.sh xcape setxkbmap xmodmap
+# initialize keymaps
 
 setxkbmap -option
-killall -e xcape 1>/dev/null 2>&1
+# killall -e xcape 1>/dev/null 2>&1
 
 if [ ! "$1" = "default" ]; then
     xmodmap "$XMODMAP"
-    xcape -e 'Alt_L=Tab;Control_L=Escape;Alt_R=backslash;Control_R=BackSpace' &
+    # xcape -e 'Alt_L=Tab;Control_L=Escape;Alt_R=backslash;Control_R=BackSpace' &
 fi
 
-xset r rate 300 50
+# xset r rate 300 50
